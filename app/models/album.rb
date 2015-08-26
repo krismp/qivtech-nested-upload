8b1493ebfb06e109c5ce83ded63c3dfaa2dbc70e
TechieDev <techiedev1987@gmail.com>
2015-08-26 16:33:05 +0530

    File upload implemented.

---
+++ app/models/album.rb
@@ -0,0 +1,4 @@
+class Album < ActiveRecord::Base
+  has_many :images
+  accepts_nested_attributes_for :images
+end
---
+++ app/models/image.rb
@@ -0,0 +1,5 @@
+class Image < ActiveRecord::Base
+  belongs_to :album
+
+  mount_uploader :image, ImageUploader
+end
